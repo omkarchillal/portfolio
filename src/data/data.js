df1ed0a4a81f85c1649793c35b83cbb845c6852d
omkar chillal <omkarchillal99@gmail.com>
2023-04-25 11:48:40 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import weatherApp from '../assets/projects/weatherApp.png'
+import messengerApp from '../assets/projects/messengerApp.png'
+import resumeBuilder from '../assets/projects/resumeBuilder.png'
+import codeEditor from '../assets/projects/codeEditor.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React weather app",
+        image:weatherApp,
+        github:"https://github.com/omkarchillal/React_Weather_App.git",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React messenger app",
+        image:messengerApp,
+        github:"https://github.com/omkarchillal/messengerApp.git",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Resume builder",
+        image:resumeBuilder,
+        github:"https://github.com/omkarchillal/Resume-builder-.git",
+        live:"",
+    },
+    {
+        id:4,
+        name:"online code editor",
+        image:codeEditor,
+        github:"https://github.com/omkarchillal/Online_Code_Editor.git",
+        live:"",
+    },
+
+
+]
